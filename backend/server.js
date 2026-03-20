4d833113e556684053b4dfa4694033d42ed9412c
ogboNoble001 <ogbonoble84@gmail.com>
2026-03-20 07:37:59 +0100

    stable version

--- backend/server.js
+++ backend/server.js
@@ -190,7 +190,7 @@ const authenticateToken = (req, res, next) => {
   const token = authHeader && authHeader.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ success: false, message: "Access token required" });
+    return res.status(401).json({ success: false, message: "Access token is required" });
   }
 
   try {
